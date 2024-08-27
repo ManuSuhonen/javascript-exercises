8fcfff4540852de09210d2e3f4c9bdca9f369f4f
Manu Suhonen <manu.suhonen@gmail.com>
2024-08-27 20:52:23 +0300

    Exercise 02 - repeatString, completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, count) {
 
+    if (count < 0) {
+        return 'ERROR';
+    }
+
+    let temp = '';
+
+    for (let i = 0; i < count; i++) {
+        temp += str;      
+    }
+    return temp;
 };
 
 // Do not edit below this line
